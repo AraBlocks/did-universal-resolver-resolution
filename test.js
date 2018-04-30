255ddf6f213efb5dd2ffd82cf7f080f1114db0ef
Joseph Werle <joseph.werle@gmail.com>
2018-04-30 11:52:37 -0400

    Release 0.1.0

--- test.js
+++ test.js
@@ -4,4 +4,9 @@ const { ResolutionResult, ResolutionError } = require('./')
 const test = require('tape')
 
 test("ResolutionResult", (t) => {
+  const resolverMetadata = new Map()
+  const driverMetadata = new Map()
+  const didReference = {}
+  const didDocument = {}
+  t.end()
 })
